156f596b658b4b66f16aa2c3a1338bf07d6fcc6a
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-12-11 21:02:07 +0200

    start

--- Lessons-99/ViewController.swift
+++ Lessons-99/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    
+    var images = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Choose a pair"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath! // Путь к папке ресурсов
+        let items = try! fm.contentsOfDirectory(atPath: path) // Получение всех файлов в папке
+        
+        for item in items {
+            // Проверяем, что файл имеет расширение .png
+            if item.hasSuffix(".png") {
+                // Путь к изображению
+                let imagePath = "\(path)/\(item)"
+                if let image = UIImage(contentsOfFile: imagePath) {
+                    images.append(image) // Добавляем изображение в массив
+                }
+            }
+        }
+        
+        print("Загружено \(images.count) изображений.")
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
+    
 }
-
